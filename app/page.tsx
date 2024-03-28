d409e9997305014b7ab1254384ea581150a5a4de
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2024-03-28 15:19:12 -0700

    Login
    Started simple login

---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+import {redirect} from 'next/navigation';
+import {getSession, login, logout} from './lib';
+
+export default function Home() {
+  const session = getSession();
+
+  return (
+    <section>
+      <form action={async (formdata)=>{
+        'use server';
+        await login(formdata);
+        redirect('/');
+      }}>
+        <input type="email" name="email" id='email' />
+        <input type="password" name="password" id="password" />
+        <button type="submit">Login</button>
+      </form>
+
+      <form action={async ()=>{
+        'use server';
+        await logout();
+        redirect('/');
+      }}>
+        <button type='submit'>Logout</button>
+      </form>
+      <pre>{JSON.stringify(session,null,2)}</pre>
+    </section>
+  );
+}
