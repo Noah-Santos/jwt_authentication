85f97486b18562c0fb4516e53c14dcf314e60bdb
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2024-04-02 15:04:55 -0700

    Fix
    Fix an issue

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 import {redirect} from 'next/navigation';
 import {getSession, login, logout} from './lib';
 
-export default function Home() {
-  const session = getSession();
+export default async function Home() {
+  const session = await getSession();
   return (
     <section>
       <form action={async (formdata)=>{
