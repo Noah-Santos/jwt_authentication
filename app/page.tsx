e392e09cca4ad788b263c8109ea8b456c5612181
Noah-Santos <noahbsantos@gmail.com>
2024-04-02 21:02:56 -0700

    Login
    Made very simple styling for the login page and the nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,23 @@
 import Image from 'next/image';
 import {redirect} from 'next/navigation';
 import {getSession, login, logout} from './lib';
+import styles from './Styles/login.module.css';
 
 export default async function Login() {
   const session = await getSession();
   return (
-    <section>
-      <form action={async (formdata)=>{
+    <section className={styles.cont}>
+      <form className={styles.form} action={async (formdata)=>{
         'use server';
         if(await login(formdata)){
           redirect('/home');
         }
       }}>
-        <input type="email" name="email" id='email' />
-        <input type="password" name="password" id="password" />
-        <button type="submit">Login</button>
+        <input type="email" name="email" id='email' placeholder='Email' className={styles.input}/>
+        <input type="password" name="password" id="password" placeholder='Password' className={styles.input}/>
+        <button type="submit" className={styles.button}>Login</button>
       </form>
 
-      <form action={async ()=>{
-        'use server';
-        await logout();
-        redirect('/');
-      }}>
-        <button type='submit'>Logout</button>
-      </form>
-      <pre>{JSON.stringify(session,null,2)}</pre>
     </section>
   );
 }
